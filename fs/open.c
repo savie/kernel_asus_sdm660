7d96ed31f818ce37a31ec3f936ab1afe69a8644e
savie <ntop_one@yahoo.co.id>
2025-01-20 07:55:28 +0700

    Revert "ksu: Integrated KernelSU into non-GKI kernel"
    This reverts commit 2258a41f11c30e7998724529e36711df91f4169c.
    
    Signed-off-by: savie <ntop_one@yahoo.co.id>

--- fs/open.c
+++ fs/open.c
@@ -34,10 +34,6 @@
 
 #include "internal.h"
 
-#ifdef CONFIG_KSU
-#include <linux/ksu.h>
-#endif
-
 int do_truncate2(struct vfsmount *mnt, struct dentry *dentry, loff_t length,
 		unsigned int time_attrs, struct file *filp)
 {
@@ -352,11 +348,6 @@ SYSCALL_DEFINE4(fallocate, int, fd, int, mode, loff_t, offset, loff_t, len)
 	return ksys_fallocate(fd, mode, offset, len);
 }
 
-#ifdef CONFIG_KSU
-extern int ksu_handle_faccessat(int *dfd, const char __user **filename_user, int *mode,
-			 int *flags);
-#endif
-
 /*
  * access() needs to use the real uid/gid, not the effective uid/gid.
  * We do this by temporarily clearing all FS-related capabilities and
@@ -371,10 +362,6 @@ long do_faccessat(int dfd, const char __user *filename, int mode)
 	struct vfsmount *mnt;
 	int res;
 	unsigned int lookup_flags = LOOKUP_FOLLOW;
-    #ifdef CONFIG_KSU
-	if (get_ksu_state() > 0)
-	    ksu_handle_faccessat(&dfd, &filename, &mode, NULL);
-    #endif
 
 	if (mode & ~S_IRWXO)	/* where's F_OK, X_OK, W_OK, R_OK? */
 		return -EINVAL;
